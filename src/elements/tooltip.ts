603bd412a0a1a921678c1d88f05260205dab54b1
Paul Jones <p-g-j@hotmail.com>
2019-02-22 21:56:58 +0000

    moved styles

--- src/elements/tooltip.ts
+++ src/elements/tooltip.ts
@@ -3,7 +3,7 @@ export class TooltipElement extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({ mode: 'open' });
+        const shadow = this.attachShadow({ mode: 'closed' });
         shadow.innerHTML = this.html;
     }
 
@@ -14,8 +14,8 @@ export class TooltipElement extends HTMLElement {
     `;
 
     private readonly html: string = `
+        <style>${this.css}</style>
         <div>
-            <style>${this.css}</style>
             <slot name="slot-1"></slot>
             <span>wootie-o</span>
             <slot name="slot-2"></slot>
