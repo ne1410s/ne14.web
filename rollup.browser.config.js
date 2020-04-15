e09f2de3d12bebfef3072bc8c90fcc02cce65c4c
Paul Jones <pjjones@siemens.com>
2020-04-15 10:22:37 +0100

    Removed demo and tidied up deps

--- rollup.browser.config.js
+++ rollup.browser.config.js
@@ -1,23 +1,17 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
-import copy from 'rollup-plugin-copy';
 import url from '@rollup/plugin-url';
 import pkg from './package.json';
 
 // UMD build (for browsers)
 export default {
-  input: 'src/demo/index.ts', // drops out demo script for browsers
+  input: 'src/index.ts',
   output: {
     name: pkg.displayName,
     file: pkg.browser,
     format: 'umd'
   },
   plugins: [
-    resolve(), // find external modules
-    commonjs(), // convert external modules to ES modules
-    copy({ targets: [{ src: 'src/demo/*.html', dest: 'dist' }] }),
     typescript(),
     terser({ include: '*.umd.min.js' }),
     url({ include: ['src/**/*.css', 'src/**/*.html'] })
--- src/demo/index.ts
+++
@@ -1,5 +0,0 @@
-import { Tooltip } from './tooltip/tooltip';
-
-if ('customElements' in window) {
-  window.customElements.define('ne14-tip', Tooltip);
-}
